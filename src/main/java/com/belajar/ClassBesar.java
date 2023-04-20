bc2c38277849beb1ad3ac228b2365c191e5bddfc
Tio Haidar Hanif <atiohaidar@gmail.com>
2023-04-20 22:51:53 +0700

    Setter based injection

--- src/main/java/com/belajar/ClassBesar.java
+++ src/main/java/com/belajar/ClassBesar.java
@@ -1,26 +1,36 @@
 package com.belajar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
-/*Constructor based injection
- * jadi ini klo misal ClassKecil nya ga kedaftar, bakal error. jadi harus ke daftar dua duannya
+/* Setter based injection
+ * jadi ini klo misal ClassKecil nya ga kedaftar di conteiner, bisa ga maslaah, masih bisa
  */
 
 
-@Component // ini tuh supaya kelas ini ke daftar di container nya, jadi kelas ini objennya otomatis dibikin oleh spring
-// klo ga di bikin anotate kek gini, ga bakala di bikin objetnya sama sprign nya
-// karena otomatis ke create objectnya, maka constractor nya itu otomatis jalan juga
+ @Component // tetep dijadii n component supaya kedaftar dan otomatis kebikin objecfctnya sma spring nya
+// ini ga perlu di kasih constructor(karena setter based)
 public class ClassBesar {
     ClassKecil kelasKecil;
-
-    public ClassBesar (ClassKecil kelasKecil){
+    
+    @Autowired(required = false) // jadi ini optional.
+    // yang aku pahami, ini kita coba nyari object kelaskecil di container, tapi klo ga ada ga apa apa.
+    // tapi klo misal ada di container, maka dimasukin ke sini, klo ga ada ga apa apa, karena ga required juga
+    public void setKelasKecil(ClassKecil kelasKecil){// jadi masukinnya make setter
         this.kelasKecil = kelasKecil;
 
     }
+
     @PostConstruct // ini anotateyang ngasih tau klo fung si ini otomatis di jjalanin setelah constructornnya di panggil
     public void sayHallo(){
+
         System.out.println("ini kelas besar");
-        kelasKecil.haloGes();
+        if (kelasKecil == null){
+                System.out.println("Kelas Kecilnnya ga terdeteksi");
+        }else{
+
+            kelasKecil.haloGes();
+        }
     }
 }
--- src/main/java/com/belajar/ClassKecil.java
+++ src/main/java/com/belajar/ClassKecil.java
@@ -2,7 +2,8 @@ package com.belajar;
 
 import org.springframework.stereotype.Component;
 
-@Component //klo kek gini berarti class ini udah ke daftar ke container dan kelas ini bakal dibikin objetnya otomatis sama sprign nya
+
+ // klo ga ada anotate component, maka ga masuk ke container
 public class ClassKecil {
     public void haloGes(){
         System.out.println("halau semua, ini dari kelas kecil");
